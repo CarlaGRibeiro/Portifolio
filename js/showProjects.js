a406ca19b87f6343181e35203f3c8fe53041add4
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2024-08-08 13:45:36 -0300

    limpeza

--- js/showProjects.js
+++
@@ -1,8 +0,0 @@
-const projects = document.getElementById("projectsList");
-const mostrar = document.getElementById("mostrar");
-
-function showProjects() {
-  projects.classList.toggle("projectsOn");
-  
-}
-mostrar.addEventListener("click", showProjects);
