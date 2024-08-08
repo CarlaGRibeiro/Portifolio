4e809ee4d2bcaab20499e1b773e45f0e847ca45f
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2024-08-08 13:45:13 -0300

    feat: mostrar serviços

--- js/actionIndex.js
+++ js/actionIndex.js
@@ -1,6 +1,11 @@
+const services = document.getElementById("mostrarP")
+const btnServices = document.getElementById("mostrarService")
+
+
 const projects = document.getElementById("projectsList");
 const mostrar = document.getElementById("mostrar");
 const btnWhats = document.getElementById("btn-whats");
+
 const urlWhats = "https://api.whatsapp.com/send?phone=5521983591846&text=Ol%C3%A1,%20vi%20seu%20portif%C3%B3lio%20e,%20gostaria%20de%20saber%20mais%20sobre%20voc%C3%AA%20e%20seus%20projetos.%20"
 function showProjects() {
   projects.classList.toggle("projectsOn");
@@ -11,4 +16,11 @@ mostrar.addEventListener("click", showProjects);
 btnWhats.addEventListener('click',()=>{
   window.open(urlWhats);
 })
+
+
+function showServices(){
+  services.classList.toggle("serviceOn");
+}
+
+btnServices.addEventListener("click", showServices);
 
